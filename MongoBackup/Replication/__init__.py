3a9964010a68da2119c12686d2240abbd8bbbe40
Tim Vaillancourt <tim@elementspace.com>
2016-10-24 17:51:41 +0200

    readme cleanup, fixed defaults in main config

--- MongoBackup/Replication/__init__.py
+++ MongoBackup/Replication/__init__.py
@@ -2,6 +2,6 @@ from Replset import Replset
 from ReplsetSharded import ReplsetSharded
 
 def config(parser):
-	parser.add_argument("-replication.max_lag_secs", dest="replication.max_lag_secs", help="Maximum replication lag of chosen backup replica(s), in seconds (default: 5)", default=5, type=int)
-	parser.add_argument("-replication.use_hidden", dest="replication.use_hidden", help="Use hidden secondary/replica members only for backup (default: false)", default=False, action="store_true")
+	parser.add_argument("-replication.max_lag_secs", dest="replication.max_lag_secs", help="Maximum replication lag of backup replica(s) in seconds (default: 5)", default=5, type=int)
+	parser.add_argument("-replication.use_hidden", dest="replication.use_hidden", help="Only use hidden secondary members for backup (default: false)", default=False, action="store_true")
 	return parser
