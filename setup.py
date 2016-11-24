bfe18c74897beba8f88bf1979ef03b6567b039f3
Tim Vaillancourt <tim@elementspace.com>
2016-11-24 13:04:29 +0100

    added long description so PyPi has docs

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 from setuptools import setup, find_packages
 
 
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
 setup(
     name='mongodb_consistent_backup',
     version='#.#.#',
     license="ASL-2",
     description='Tool for getting consistent backups from MongoDB Clusters and ReplicaSet',
+    long_description=readme(),
     author='Percona-Lab',
     author_email='tim.vaillancourt@percona.com',
     url='https://github.com/Percona-Lab/mongodb_consistent_backup',
