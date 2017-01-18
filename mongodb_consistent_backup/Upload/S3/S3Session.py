fc19095265213d907b7382ba056a1b97619e9e67
Tim Vaillancourt <tim@timvaillancourt.com>
2017-01-18 19:38:07 +0100

    adding s3 back in

---
+++ mongodb_consistent_backup/Upload/S3/S3Session.py
@@ -0,0 +1,53 @@
+import logging
+
+import boto
+import boto.s3
+
+
+class S3Session:
+    def __init__(self, access_key, secret_key, s3_host='s3.amazonaws.com', secure=True, num_retries=5, socket_timeout=15):
+        self.access_key     = access_key
+        self.secret_key     = secret_key
+        self.s3_host        = s3_host
+        self.secure         = secure
+        self.num_retries    = num_retries
+        self.socket_timeout = socket_timeout
+
+        for section in boto.config.sections():
+            boto.config.remove_section(section)
+        boto.config.add_section('Boto')
+        boto.config.setbool('Boto', 'is_secure', self.secure)
+        boto.config.set('Boto', 'http_socket_timeout', str(self.socket_timeout))
+        boto.config.set('Boto', 'num_retries', str(self.num_retries))
+
+        self._conn = None
+        self.connect()
+
+    def close(self):
+        if not self._conn:
+            self._conn.close()
+        pass
+
+    def connect(self):
+        if not self._conn:
+            try:
+                logging.debug("Connecting to AWS S3 with Access Key: %s" % self.access_key)
+                self._conn = boto.s3.S3Connection(
+                    self.access_key,
+                    self.secret_key,
+                    host=self.s3_host,
+                    is_secure=self.secure
+                )
+                logging.debug("Successfully connected to AWS S3 with Access Key: %s" % self.access_key)
+            except Exception, e:
+                logging.error("Cannot connect to AWS S3 with Access Key: %s!" % self.access_key)
+                raise e
+        return self._conn
+
+    def get_bucket(self, bucket_name):
+        try:
+            logging.debug("Connecting to AWS S3 Bucket: %s" % bucket_name)
+            return self._conn.get_bucket(bucket_name)
+        except Exception, e:
+            logging.error("Cannot connect to AWS S3 Bucket: %s!" % bucket_name)
+            raise e
---
+++ mongodb_consistent_backup/Upload/S3/S3UploadThread.py
@@ -0,0 +1,40 @@
+import logging
+
+from filechunkio import FileChunkIO
+from S3Session import S3Session
+
+
+class S3UploadThread:
+    def __init__(self, bucket_name, access_key, secret_key, s3_host, multipart_id, part_num, file_name, offset,
+                 byte_count, retries=5, secure=True):
+        self.bucket_name  = bucket_name
+        self.access_key   = access_key
+        self.secret_key   = secret_key
+        self.s3_host      = s3_host
+        self.multipart_id = multipart_id
+        self.part_num     = part_num
+        self.file_name    = file_name
+        self.offset       = offset
+        self.byte_count   = byte_count
+        self.retries      = retries
+        self.secure       = secure
+
+        try:
+            self.s3_conn = S3Session(self.access_key, self.secret_key, self.s3_host, self.secure, self.retries)
+            self.bucket  = self.s3_conn.get_bucket(self.bucket_name)
+        except Exception, e:
+            logging.error("Could not get AWS S3 connection to bucket %s! Error: %s" % (self.bucket_name, e))
+            raise e
+
+    def run(self):
+        try:
+            for mp in self.bucket.get_all_multipart_uploads():
+                if mp.id == self.multipart_id:
+                    logging.info("Uploading file: %s (part num: %s)" % (self.file_name, self.part_num))
+                    with FileChunkIO(self.file_name, 'r', offset=self.offset, bytes=self.byte_count) as fp:
+                        mp.upload_part_from_file(fp=fp, part_num=self.part_num)
+                    logging.debug("Uploaded file: %s (part num: %s)" % (self.file_name, self.part_num))
+                    break
+        except Exception, e:
+            logging.error("AWS S3 multipart upload failed after %i retries! Error: %s" % (self.retries, e))
+            raise e
