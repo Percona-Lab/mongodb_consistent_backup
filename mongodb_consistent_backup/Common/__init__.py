c2d85cd34b4b8a076fb8448d769c5631c1b21959
Tim Vaillancourt <tim@timvaillancourt.com>
2017-04-13 13:13:23 +0200

    Don't print mongodump stderr if the line is empty (date only), pt 3

--- mongodb_consistent_backup/Common/__init__.py
+++ mongodb_consistent_backup/Common/__init__.py
@@ -4,4 +4,4 @@ from LocalCommand import LocalCommand
 from Lock import Lock
 from MongoUri import MongoUri
 from Timer import Timer
-from Util import config_to_string, parse_method, validate_hostname 
+from Util import config_to_string, is_datetime, parse_method, validate_hostname 
