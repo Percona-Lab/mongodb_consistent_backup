5bcc6ce87d34ddf4f3e4c875039a56116186bb24
Tim Vaillancourt <tim@timvaillancourt.com>
2017-06-16 04:10:56 +0200

    better comment for usage

--- scripts/kill_tailer_thread.js
+++ scripts/kill_tailer_thread.js
@@ -1,5 +1,10 @@
 // A script for failure testing the oplog tailer.
 // Kills running mongodb_consistent_backup.Oplog.Tailer.TailThread threads.
+//
+// Usage:
+// $ mongo --shell /path/to/mongodb_consistent_backup/scripts/kill_tailer.thread.js
+// > mcbKillTailer()
+// ...
 function mcbKillTailer() {
 	return db.currentOp().inprog.forEach(
 		function(x) {
