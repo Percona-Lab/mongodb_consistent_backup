0101a54d8d2dc3da8d2bb9778b1f45b139832a22
Tim Vaillancourt <tim@timvaillancourt.com>
2017-08-21 21:44:53 +0200

    cache docker build so other stages can use it

--- scripts/travis-ci/test-cluster/run.sh
+++ scripts/travis-ci/test-cluster/run.sh
@@ -8,6 +8,37 @@ MONGO_VERSION=${1:-3.2}
 pushd $(dirname $0)
 	sed s/'{{MONGO_VERSION}}'/$MONGO_VERSION/g docker-compose.yml.tmpl >docker-compose.yml
 
-	echo "# Starting mongodb_consistent_backup (in docker)"
-	docker-compose up mongodb_consistent_backup
+	echo "# Starting instances with docker-compose"
+	docker-compose up -d mongo-mongos
+	
+	echo "# Initiating csReplSet (config server set)"
+	docker-compose run --rm mongo-csReplSet-1 mongo mongo-csReplSet-1:27019 --quiet --eval 'rs.initiate({
+	  _id: "csReplSet",
+	  configsvr: true,
+	  members: [
+	    { _id: 0, host: "mongo-csReplSet-1:27019" },
+	    { _id: 1, host: "mongo-csReplSet-2:27029" }
+	  ]
+	})'
+	
+	echo "# Initiating rs0"
+	docker-compose run --rm mongo-rs0-1 mongo mongo-rs0-1:27017 --quiet --eval 'rs.initiate({
+	  _id: "rs0",
+	  members: [
+	    { _id: 0, host: "mongo-rs0-1:27017" },
+	    { _id: 1, host: "mongo-rs0-2:27027", priority: 0 }
+	  ]
+	})'
+	
+	echo "# Waiting 15 seconds"
+	sleep 15
+	
+	echo "# Adding shard rs0"
+	docker-compose run --rm mongo-mongos mongo mongo-mongos:27018 --quiet --eval 'sh.addShard("rs0/mongo-rs0-1:27017,mongo-rs0-2:27027")'
+
+        echo "# Starting mongodb_consistent_backup (in docker)"
+        docker-compose up mongodb_consistent_backup
+
+        echo "# Stopping instances with docker-compose"
+        docker-compose down
 popd
