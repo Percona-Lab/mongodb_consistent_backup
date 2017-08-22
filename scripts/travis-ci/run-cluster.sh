856835964df093777da7f08962b1a42ff2a960e9
Tim Vaillancourt <tim@timvaillancourt.com>
2017-08-22 16:08:22 +0200

    Support SCCC mode clusters

--- scripts/travis-ci/run-cluster.sh
+++ scripts/travis-ci/run-cluster.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 MONGO_VERSION=${1:-3.2}
-MCB_EXTRA="${@:2}"
+CONFIGSVR_TYPE=${2:-CSRS}
+MCB_EXTRA="${@:3}"
 
 pushd $(dirname $0)
 	source $PWD/func.sh
@@ -12,21 +13,34 @@ pushd $(dirname $0)
 	export MONGO_VERSION=${MONGO_VERSION}
 	export MCB_EXTRA=${MCB_EXTRA}
 
+	export CONFIGSVR_REPLSET=csReplSet
+	if [ "${CONFIGSVR_TYPE}" == "CSRS" ]; then
+		export CONFIGSVR_FLAGS="--replSet ${CONFIGSVR_REPLSET}"
+		export MONGOS_CONFIGDB="${CONFIGSVR_REPLSET}/mongo-cs-1:27017,mongo-cs-2:27017"
+		echo "# Using CSRS-based config servers: ${MONGOS_CONFIGDB}"
+	else
+		export CONFIGSVR_FLAGS=
+		export MONGOS_CONFIGDB="mongo-cs-1:27017,mongo-cs-2:27017"
+		echo "# Using SCCC-based config servers: ${MONGOS_CONFIGDB}"
+	fi
+
 	echo "# Starting instances with docker-compose"
 	docker-compose up -d mongo-mongos
 
 	echo "# Waiting 10 seconds"
 	sleep 10
 	
-	echo "# Initiating csReplSet (config server set)"
-	doMongo mongo-mongos mongo-cs-1:27017 'rs.initiate({
-	  _id: "csReplSet",
-	  configsvr: true,
-	  members: [
-	    { _id: 0, host: "mongo-cs-1:27017" },
-	    { _id: 1, host: "mongo-cs-2:27017" }
-	  ]
-	})'
+	if [ "${CONFIGSVR_TYPE}" == "CSRS" ]; then
+		echo "# Initiating csReplSet (config server set)"
+		doMongo mongo-mongos mongo-cs-1:27017 'rs.initiate({
+		  _id: "csReplSet",
+		  configsvr: true,
+		  members: [
+		    { _id: 0, host: "mongo-cs-1:27017" },
+		    { _id: 1, host: "mongo-cs-2:27017" }
+		  ]
+		})'
+	fi
 	
 	echo "# Initiating rs0"
 	doMongo mongo-mongos mongo-s-rs0-1:27017 'rs.initiate({
