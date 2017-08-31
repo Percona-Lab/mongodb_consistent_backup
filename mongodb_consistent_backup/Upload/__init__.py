e8fe891747e82144ef8dcd55a850188c71037d1b
Tim Vaillancourt <tim@timvaillancourt.com>
2017-08-31 17:33:19 +0200

    support threaded rsync upload

--- mongodb_consistent_backup/Upload/__init__.py
+++ mongodb_consistent_backup/Upload/__init__.py
@@ -2,6 +2,6 @@ from Upload import Upload  # NOQA
 
 
 def config(parser):
-    parser.add_argument("--upload.method", dest="upload.method", help="Uploader method (default: none)", default='none', choices=['gs', 's3', 'none'])
+    parser.add_argument("--upload.method", dest="upload.method", help="Uploader method (default: none)", default='none', choices=['gs', 'rsync', 's3', 'none'])
     parser.add_argument("--upload.remove_uploaded", dest="upload.remove_uploaded", help="Remove source files after successful upload (default: false)", default=False, action="store_true")
     return parser
