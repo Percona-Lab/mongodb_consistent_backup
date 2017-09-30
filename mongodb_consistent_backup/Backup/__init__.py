3bb56983430e3e961c9ad2b2314546eeb1569cc5
Tim Vaillancourt <tim@timvaillancourt.com>
2017-09-30 17:12:30 +0200

    Flake8 fixes, enable long-line errors but set length to 160

--- mongodb_consistent_backup/Backup/__init__.py
+++ mongodb_consistent_backup/Backup/__init__.py
@@ -2,7 +2,10 @@ from Backup import Backup  # NOQA
 
 
 def config(parser):
-    parser.add_argument("-n", "--backup.name", dest="backup.name", help="Name of the backup set (default: default)", default='default', type=str)
-    parser.add_argument("-l", "--backup.location", dest="backup.location", help="Base path to store the backup data (default: /var/lib/mongodb-consistent-backup)", default='/var/lib/mongodb-consistent-backup', type=str)
-    parser.add_argument("-m", "--backup.method", dest="backup.method", help="Method to be used for backup (default: mongodump)", default='mongodump', choices=['mongodump'])
+    parser.add_argument("-n", "--backup.name", dest="backup.name", default='default', type=str,
+                        help="Name of the backup set (default: default)")
+    parser.add_argument("-l", "--backup.location", dest="backup.location", default='/var/lib/mongodb-consistent-backup', type=str,
+                        help="Base path to store the backup data (default: /var/lib/mongodb-consistent-backup)")
+    parser.add_argument("-m", "--backup.method", dest="backup.method", default='mongodump', choices=['mongodump'],
+                        help="Method to be used for backup (default: mongodump)")
     return parser
