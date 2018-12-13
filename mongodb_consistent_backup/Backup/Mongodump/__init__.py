1863242ced345c65746014c28cf8224719bc2eb8
Daniel Schneller <ds@danielschneller.de>
2018-12-13 10:53:51 +0100

    Add "--backup.mongodump.force_table_scan" option
    If set, this enables mongodump's `--forceTableScan` option which
    can speed up dumps significantly, especially when custom id fields
    are used or when the id values are large.
    
    According to the discussion in [TOOLS-845](https://jira.mongodb.org/browse/TOOLS-845)
    this is safe to do for WiredTiger.
    
    * https://jira.mongodb.org/browse/TOOLS-845?focusedCommentId=988298&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-988298
    * https://jira.mongodb.org/browse/TOOLS-845?focusedCommentId=988414&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-988414
    
    Defaults to false.

--- mongodb_consistent_backup/Backup/Mongodump/__init__.py
+++ mongodb_consistent_backup/Backup/Mongodump/__init__.py
@@ -10,4 +10,11 @@ def config(parser):
     parser.add_argument("--backup.mongodump.threads", dest="backup.mongodump.threads",
                         help="Number of threads to use for each mongodump process. There is 1 x mongodump per shard, be careful! (default: shards/CPUs)",
                         default=0, type=int)
+    # for a detailled explanation see
+    # see https://jira.mongodb.org/browse/TOOLS-845?focusedCommentId=988298&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-988298
+    # and https://jira.mongodb.org/browse/TOOLS-845?focusedCommentId=988414&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-988414
+    parser.add_argument("--backup.mongodump.force_table_scan", dest="backup.mongodump.force_table_scan", default=False,
+                        action="store_true", help="Enables mongodump's forceTableScan option. Useful if custom ids are "
+                                                  "used or id values are large. ONLY use with WiredTiger! (default: false)")
+
     return parser
